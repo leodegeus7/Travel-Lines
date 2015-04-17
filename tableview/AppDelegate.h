8ec680399042c5178b504d2d9fb455bf4bcc1795
Leonardo Geus <leonardodegeus@gmail.com>
2015-04-17 16:12:58 -0300

    Primeiro comia

---
+++ tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableview
+//
+//  Created by Leonardo Geus on 24/03/15.
+//  Copyright (c) 2015 Leonardo Geus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableview
+//
+//  Created by Leonardo Geus on 24/03/15.
+//  Copyright (c) 2015 Leonardo Geus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
